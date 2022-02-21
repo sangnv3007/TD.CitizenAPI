3f1211b813ac260f88f75fbdc8d8dc2096c5b2c4
DarkangelKid <tunglamnguyen91@gmail.com>
2022-02-21 09:33:50 +0700

    fix

--- src/Core/Application/Catalog/Feedbacks/CreateFeedbackRequest.cs
+++ src/Core/Application/Catalog/Feedbacks/CreateFeedbackRequest.cs
@@ -8,22 +8,23 @@ public partial class CreateFeedbackRequest : IRequest<Result<Guid>>
     public string? Content { get; set; }
 
     public class CreateHomePageInforRequestValidator : CustomValidator<CreateFeedbackRequest>
-{
-    public CreateHomePageInforRequestValidator(IReadRepository<HomePageInfor> repository, IStringLocalizer<CreateHomePageInforRequestValidator> localizer) =>
-        RuleFor(p => p.UserName).NotEmpty();
-}
+    {
+        public CreateHomePageInforRequestValidator(IReadRepository<HomePageInfor> repository, IStringLocalizer<CreateHomePageInforRequestValidator> localizer) =>
+            RuleFor(p => p.UserName).NotEmpty();
+    }
 
     public class CreateFeedbackRequestHandler : IRequestHandler<CreateFeedbackRequest, Result<Guid>>
-{
-    // Add Domain Events automatically by using IRepositoryWithEvents
-    private readonly IRepositoryWithEvents<Feedback> _repository;
-
-    public CreateFeedbackRequestHandler(IRepositoryWithEvents<Feedback> repository) => _repository = repository;
-
-    public async Task<Result<Guid>> Handle(CreateFeedbackRequest request, CancellationToken cancellationToken)
     {
-        var item = new Feedback(request.UserName,request.Rate, request.Description, request.Content, 0);
-        await _repository.AddAsync(item, cancellationToken);
-        return Result<Guid>.Success(item.Id);
+        // Add Domain Events automatically by using IRepositoryWithEvents
+        private readonly IRepositoryWithEvents<Feedback> _repository;
+
+        public CreateFeedbackRequestHandler(IRepositoryWithEvents<Feedback> repository) => _repository = repository;
+
+        public async Task<Result<Guid>> Handle(CreateFeedbackRequest request, CancellationToken cancellationToken)
+        {
+            var item = new Feedback(request.UserName, request.Rate, request.Description, request.Content, 0);
+            await _repository.AddAsync(item, cancellationToken);
+            return Result<Guid>.Success(item.Id);
+        }
     }
 }
